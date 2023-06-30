bf3c5a634d74726f7eda6958a4c6ab832e155ba2
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-06-30 14:48:44 +0300

    Palindrome test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function reversed(word){
+  return word.split('').reverse().join('')
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+ const reversedWord = reversed(word)
+ return reversedWord === word
 }
 
 /* 
@@ -13,6 +19,7 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
